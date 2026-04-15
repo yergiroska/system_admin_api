f2935552188294a20e96ebfb8d1a61307e5bb6f2
Yergiroska Aguirre <yergiroska77@gmail.com>
2026-04-15 08:17:46 +0200

    Modificar database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
-import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
 
 engine = create_engine(DATABASE_URL)
-SQLAlchemyInstrumentor().instrument(engine=engine)
+
+if ENVIRONMENT == "local":
+    try:
+        from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+        SQLAlchemyInstrumentor().instrument(engine=engine)
+    except ImportError:
+        pass
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
